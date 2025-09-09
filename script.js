dfb010701ee3d7799060db60ff9a5553eadf2576
AnkitChavda023 <122983801+AnkitChavda023@users.noreply.github.com>
2025-09-09 21:10:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+ 
+  
+    const lastModified = new Date(document.lastModified);
+ 
+    const formattedDate = lastModified.toLocaleString('en-US', {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit'
+    });
+
+    document.getElementById("lastUpdated").innerHTML += formattedDate;
+
+        function startTime() {
+        const today = new Date();
+        let h = today.getHours();
+        let m = today.getMinutes();
+        let s = today.getSeconds();
+        m = checkTime(m);
+        s = checkTime(s);
+        document.getElementById('txt').innerHTML = today 
+        setTimeout(startTime, 1000);
+        }
+
+        function checkTime(i) {
+        if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+        return i;
+    }
